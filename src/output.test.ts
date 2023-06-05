3336c6552254afb0e521224d6f7c10684631b8cc
Clément 'birdy' Danjou <clement@danjou.io>
2023-06-05 15:28:15 +0200

    add npmignore and clean repo

--- src/output.test.ts
+++ src/output.test.ts
@@ -1,20 +1,38 @@
 import { EIP712, generateToken } from './output';
 
 describe('Output', () => {
-  it('encrypt and decrypt one character', async () => {
+  it('creates a valid EIP712 object', async () => {
+    const response = await generateToken({
+      verifyingContract: '0xccc',
+      chainId: 1234,
+      name: 'hello',
+      version: '3',
+    });
+    const jsonToken: EIP712 = response.eip712;
+    expect(jsonToken.domain.chainId).toBe(1234);
+    expect(jsonToken.domain.name).toBe('hello');
+    expect(jsonToken.domain.version).toBe('3');
+    expect(jsonToken.message.publicKey).toBe(response.keypair.publicKey);
+
+    expect(jsonToken.primaryType).toBe('Reencrypt');
+    expect(jsonToken.types.Reencrypt.length).toBe(1);
+    expect(jsonToken.types.Reencrypt[0].name).toBe('publicKey');
+    expect(jsonToken.types.Reencrypt[0].type).toBe('bytes32');
+  });
+
+  it('creates a valid EIP712 object with default values', async () => {
     const response = await generateToken({
       verifyingContract: '0xccc',
     });
-    console.log(response);
     const jsonToken: EIP712 = response.eip712;
     expect(jsonToken.domain.chainId).toBe(9000);
-    expect(jsonToken.domain.name).toBe('Authorization for 0xccc');
+    expect(jsonToken.domain.name).toBe('Authorization token');
     expect(jsonToken.domain.version).toBe('1');
     expect(jsonToken.message.publicKey).toBe(response.keypair.publicKey);
 
     expect(jsonToken.primaryType).toBe('Reencrypt');
     expect(jsonToken.types.Reencrypt.length).toBe(1);
     expect(jsonToken.types.Reencrypt[0].name).toBe('publicKey');
-    expect(jsonToken.types.Reencrypt[0].type).toBe('string');
+    expect(jsonToken.types.Reencrypt[0].type).toBe('bytes32');
   });
 });
