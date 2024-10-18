48b2ece223145ce985b572aa8428f332523c2132
Clément 'birdy' Danjou <clement@danjou.io>
2024-10-18 17:34:42 +0200

    fix: update url

--- src/sdk/network.ts
+++ src/sdk/network.ts
@@ -1,18 +1,34 @@
 import { CompactPkePublicParams, TfheCompactPublicKey } from 'node-tfhe';
-import type { Eip1193Provider } from 'ethers';
 import { fromHexString } from '../utils';
 
 export type GatewayKeysItem = {
   url: string;
   signatures: string[];
 };
+
+export type GatewayKey = {
+  data_id: string;
+  param_choices: number;
+  signatures: string[];
+  urls: string[];
+};
+
 export type GatewayKeys = {
-  keyId: string;
-  crsId: string;
-  publicKey: GatewayKeysItem;
-  bootstrapKey: GatewayKeysItem;
-  crs: {
-    [key: string]: GatewayKeysItem;
+  response: {
+    fhe_key_info: {
+      fhe_public_key: GatewayKey[];
+      fhe_server_key: GatewayKey[];
+    }[];
+    verf_public_key: {
+      key_id: string;
+      server_id: number;
+      verf_public_key_address: string;
+      verf_public_key_url: string;
+    }[];
+    crs: {
+      [key: string]: GatewayKeysItem;
+    };
+    status: string;
   };
 };
 
@@ -24,8 +40,10 @@ export const getKeysFromGateway = async (url: string) => {
     }
     const data: GatewayKeys = await response.json();
     if (data) {
-      const publicKey = await (await fetch(data.publicKey.url)).text();
-      const crs2048 = await (await fetch(data.crs['2048'].url)).text();
+      const pubKeyUrl = data.response.fhe_key_info[0].fhe_public_key[0].urls[0];
+      const publicKey = await (await fetch(pubKeyUrl)).text();
+      const crsUrl = data.response.crs['2048'].url;
+      const crs2048 = await (await fetch(crsUrl)).text();
       return {
         publicKey: TfheCompactPublicKey.deserialize(fromHexString(publicKey)),
         publicParams: {
