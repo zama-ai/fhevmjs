4186363a5c2b33164441cbfbbf5f114f84ceac9a
rudy <rudy.sicard@gmail.com>
2025-03-05 15:00:34 +0100

    feat: checks handles returned by relayer/input-proof

--- src/sdk/encrypt.test.ts
+++ src/sdk/encrypt.test.ts
@@ -1,17 +1,27 @@
 import { ProvenCompactCiphertextList } from 'node-tfhe';
-import { createEncryptedInput } from './encrypt';
+import { createEncryptedInput, ZKInput } from './encrypt';
 import { publicKey, publicKeyId, publicParams } from '../test';
 import fetchMock from '@fetch-mock/core';
 
 const relayer_url = 'https://test-httpz-relayer';
 
-fetchMock.post(`${relayer_url}/input-proof`, {
-  response: {
-    handles: ['2323beef', '2234beef'],
+const auto_mock = (input: ZKInput) => {
+  const ciphertextWithZKProof = Buffer.from(input._prove()).toString('hex');
+  const options = {
+    params: { ciphertextWithZkpok: ciphertextWithZKProof },
+  };
+  const handles = input
+    ._handles()
+    .map((handle: Uint8Array) => Buffer.from(handle).toString('hex'));
+  const response = {
+    handles: handles,
     signatures: ['dead3232'],
-  },
-  status: 'success',
-});
+  };
+  fetchMock.postOnce(`${relayer_url}/input-proof`, {
+    options: options,
+    response: response,
+  });
+};
 
 describe('encrypt', () => {
   it('encrypt/decrypt', async () => {
@@ -34,9 +44,10 @@ describe('encrypt', () => {
     input.add128(BigInt(233938932390));
     input.addAddress('0xa5e1defb98EFe38EBb2D958CEe052410247F4c80');
     input.add256(BigInt('2339389323922393930'));
+    auto_mock(input);
     const { inputProof, handles } = await input.encrypt();
     expect(inputProof).toBeDefined();
-    expect(handles.length).toBe(2);
+    expect(handles.length).toBe(9);
     // const compactList = ProvenCompactCiphertextList.safe_deserialize(
     //   buffer.inputProof,
     //   BigInt(1024 * 1024 * 512),
@@ -62,9 +73,10 @@ describe('encrypt', () => {
       '0xa5e1defb98EFe38EBb2D958CEe052410247F4c80',
     );
     input.add128(BigInt(0));
+    auto_mock(input);
     const { inputProof, handles } = await input.encrypt();
     expect(inputProof).toBeDefined();
-    expect(handles.length).toBe(2);
+    expect(handles.length).toBe(1);
     // const compactList = ProvenCompactCiphertextList.safe_deserialize(
     //   buffer.inputProof,
     //   BigInt(1024 * 1024 * 512),
@@ -91,9 +103,10 @@ describe('encrypt', () => {
     const data = new Uint8Array(256);
     data.set([255], 63);
     input.addBytes256(data);
+    auto_mock(input);
     const { handles, inputProof } = await input.encrypt();
     expect(inputProof).toBeDefined();
-    expect(handles.length).toBe(2);
+    expect(handles.length).toBe(1);
     // const compactList = ProvenCompactCiphertextList.safe_deserialize(
     //   buffer.inputProof,
     //   BigInt(1024 * 1024 * 512),
@@ -198,6 +211,65 @@ describe('encrypt', () => {
     input2.addBytes256(new Uint8Array(256));
     expect(() => input2.addBool(false)).toThrow(
       'Packing more than 2048 bits in a single input ciphertext is unsupported',
+    );
+  });
+
+  it('throws if incorrect handles list size', async () => {
+    const input = createEncryptedInput(
+      '0x325ea1b59F28e9e1C51d3B5b47b7D3965CC5D8C8',
+      1234,
+      relayer_url,
+      publicKey,
+      publicParams,
+    )(
+      '0x8ba1f109551bd432803012645ac136ddd64dba72',
+      '0xa5e1defb98EFe38EBb2D958CEe052410247F4c80',
+    );
+    input.add128(BigInt(0));
+    auto_mock(input);
+    const input2 = createEncryptedInput(
+      '0x325ea1b59F28e9e1C51d3B5b47b7D3965CC5D8C8',
+      1234,
+      relayer_url,
+      publicKey,
+      publicParams,
+    )(
+      '0x8ba1f109551bd432803012645ac136ddd64dba72',
+      '0xa5e1defb98EFe38EBb2D958CEe052410247F4c80',
+    );
+    input2.add128(BigInt(0));
+    input2.add128(BigInt(0));
+    await expect(input2.encrypt()).rejects.toThrow(
+      'Incorrect Handles list sizes: (expected) 2 != 1 (received)',
+    );
+  });
+
+  it('throws if incorrect handle', async () => {
+    const input = createEncryptedInput(
+      '0x325ea1b59F28e9e1C51d3B5b47b7D3965CC5D8C8',
+      1234,
+      relayer_url,
+      publicKey,
+      publicParams,
+    )(
+      '0x8ba1f109551bd432803012645ac136ddd64dba72',
+      '0xa5e1defb98EFe38EBb2D958CEe052410247F4c80',
+    );
+    input.add128(BigInt(1));
+    auto_mock(input);
+    const input2 = createEncryptedInput(
+      '0x325ea1b59F28e9e1C51d3B5b47b7D3965CC5D8C8',
+      1234,
+      relayer_url,
+      publicKey,
+      publicParams,
+    )(
+      '0x8ba1f109551bd432803012645ac136ddd64dba72',
+      '0xa5e1defb98EFe38EBb2D958CEe052410247F4c80',
+    );
+    input2.add128(BigInt(1));
+    await expect(input2.encrypt()).rejects.toThrow(
+      /Incorrect Handle 0: \(expected\) [0-9a-z]{64} != [0-9a-z]{64} \(received\)/,
     );
   });
 });
